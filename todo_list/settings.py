dcfc852be5c799042fe8d81875eaeee1a22ba421
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-25 11:18:07 +0300

    add crispy forms for bootstrap5

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "tasks",
 ]
 
@@ -71,6 +73,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "todo_list.wsgi.application"
 

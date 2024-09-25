e549b001303b8af0f90bbf6fd8244a62adbfa114
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-25 15:01:41 +0300

    add admin panel urls and tasks app urls to global urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("tasks.urls", namespace="tasks")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("admin/", admin.site.urls),
 ]

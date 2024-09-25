57ef7a2fc2bb4fa47b77746b6c29a4baf8df6831
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-25 11:18:31 +0300

    add prepared models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    finish_mark = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["-finish_mark", "-datetime"]

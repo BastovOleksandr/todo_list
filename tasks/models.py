77a87d0d40833a276c949c863673d73e46ba4059
Olexandr Bastov <iamlucky1990@gmail.com>
2024-09-25 14:59:19 +0300

    add models.py final version

--- tasks/models.py
+++ tasks/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
-    content = models.CharField(max_length=255)
-    datetime = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
-    finish_mark = models.BooleanField(default=False)
+    is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
+    def __str__(self):
+        return self.content
+
     class Meta:
-        ordering = ["-finish_mark", "-datetime"]
+        verbose_name_plural = "dish types"
+        ordering = ["-is_done", "-created_at"]
